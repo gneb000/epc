8c54598e1df8cbc43bc598d3cfb0ce6318dd4fa5
gneb000 <gneb000@disroot.org>
2023-01-29 18:09:37 +0100

    Input parsing + EPUB navigation implemented

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
-use std::fs;
+/// Returns the page count of an epub file (based on 2000 chars per page).
 
-const CHARS_PER_PAGE: usize = 1900;
+use std::io::stdin;
+use epub::doc::EpubDoc;
 
-fn main() {
-    let epub_path = "./test_epub/test/OEBPS/Text";
+const CHARS_PER_PAGE: usize = 2000;
 
-    let files= get_file_list(epub_path);
+fn count_pages(epub_path: &str) -> usize {
+    let mut doc = EpubDoc::new(epub_path).unwrap();
+    let mut spine = doc.spine.clone();
 
-    let page_count = count_pages(files);
-    println!("{}", page_count);
-}
-
-/// Return file list in provided directory path
-fn get_file_list(dir_path: &str) -> Vec<String> {
-    fs::read_dir(dir_path).unwrap()
-        .map(|f| f.unwrap().path().display().to_string())
-        .collect()
-}
-
-/// Iterate through provided file list and return page count
-fn count_pages(file_list: Vec<String>) -> usize {
     let mut char_count = 0;
-    for file in file_list.iter() {
-        char_count += fs::read_to_string(file)
-            .expect(&format!("Unable to read the file: {}", file))
-            .chars()
-            .filter(|s| *s!='\n') // Exclude line breaks from char count
-            .count();
+    for res_id in spine.iter_mut() {
+        let content = doc.get_resource_str(res_id).unwrap().0;
+        char_count += content.chars().filter(|s| *s!='\n').count();
     }
     char_count / CHARS_PER_PAGE
+}
+
+fn main() {
+    let mut input_stream = String::new();
+    stdin().read_line(&mut input_stream).expect("Invalid input");
+    let page_count = count_pages(&input_stream.trim());
+    println!("{}", page_count);
 }
